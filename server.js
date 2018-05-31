2261b7fe728f109729817df192136e0af28ca98c
caglarpanus <panuscaglar@gmail.com>
2018-05-31 13:51:44 -0400

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const bodyParser =require("body-parser");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const sslRedirect = require("heroku-ssl-redirect");
 
 const PORT = process.env.PORT || 3001;
@@ -11,20 +11,21 @@ const app = express();
 const server = http.createServer(app);
 
 
-// mongoose.Promise = require('bluebird');
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolio', { promiseLibrary: require('bluebird') })
-//   .then(() =>  console.log('connection succesful'))
-//   .catch((err) => console.error(err));
+mongoose.Promise = require('bluebird');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolio', { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
 
 // SSL Redirect
 app.use(sslRedirect());
 
-// Set Static Folder
-app.use(express.static("client/build"));
+// // Set Static Folder
+// app.use(express.static("client/build"));
 
-// //BodyParser
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({'extended':'false'}));
+//BodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -32,6 +33,20 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  console.log('YOU ARE IN THE PRODUCTION ENV');
+  app.use('/static', express.static(path.join(__dirname, '../build/static')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/'));
+  });
+}
 
 server.listen(PORT, function() {
 console.log(`🌎 ==> Server now on port ${PORT}!`)
