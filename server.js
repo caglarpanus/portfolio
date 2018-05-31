ed9767858aebab5150f1644b42283f6063e7a6aa
caglarpanus <panuscaglar@gmail.com>
2018-05-31 12:37:16 -0400

    server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const bodyParser =require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const sslRedirect = require("heroku-ssl-redirect");
 
 const PORT = process.env.PORT || 3001;
@@ -11,10 +11,10 @@ const app = express();
 const server = http.createServer(app);
 
 
-mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolio', { promiseLibrary: require('bluebird') })
-  .then(() =>  console.log('connection succesful'))
-  .catch((err) => console.error(err));
+// mongoose.Promise = require('bluebird');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/portfolio', { promiseLibrary: require('bluebird') })
+//   .then(() =>  console.log('connection succesful'))
+//   .catch((err) => console.error(err));
 
 // SSL Redirect
 app.use(sslRedirect());
@@ -22,9 +22,9 @@ app.use(sslRedirect());
 // Set Static Folder
 app.use(express.static("client/build"));
 
-//BodyParser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended':'false'}));
+// //BodyParser
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({'extended':'false'}));
 
 // Send every request to the React app
 // Define any API routes before this runs
